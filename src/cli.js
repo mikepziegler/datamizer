e76bd23a501aa64ee915e05dcda03478674701b6
MikeZyeman <mi.zyeman@gmail.com>
2021-10-18 08:39:21 +0200

    added npm package commander for building better commands

--- src/cli.js
+++ src/cli.js
@@ -1,3 +1,21 @@
 #!/usr/bin/env node
 
-console.log("Hello World!")
+const version = require('../package.json').version;
+
+const { program } = require('commander');
+program.version(version);
+
+console.log("Hello World!");
+
+program
+    .option('-d, --debug', 'output extra debugging')
+    .option('-s, --small', 'small pizza size')
+    .option('-p, --pizza-type <type>', 'flavour of pizza');
+
+program.parse(process.argv);
+
+const options = program.opts();
+if (options.debug) console.log(options);
+console.log('pizza details:');
+if (options.small) console.log('- small pizza size');
+if (options.pizzaType) console.log(`- ${options.pizzaType}`);
